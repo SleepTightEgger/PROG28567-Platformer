13b6e0f9b7ba47d893c63e9976b836777ded82de
Tarell <thomtare@shernet.sheridancollege.ca>
2025-12-07 18:14:34 -0500

    Task 1 - Basic Movement - Added acceleration and deceleration variables - Added velocity - Made it so playerInput is mapped to "Horizontal" axis - Moved player input outside of Update() - Updated MovementUpdate to calculate velocity based on player input.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector2 velocity;
+
+    public Rigidbody2D body;
+
+    private float acceleration;
+    private float deceleration;
+    private float accelerationTime = 1f;
+    private float decelerationTime = 1f;
+
+    private float maxSpeed = 10f;
+
+    Vector2 playerInput = new Vector2();
+
     public enum FacingDirection
     {
         left, right
@@ -10,7 +24,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        acceleration = maxSpeed/accelerationTime;
+        deceleration = maxSpeed/decelerationTime;
     }
 
     // Update is called once per frame
@@ -19,13 +34,32 @@ public class PlayerController : MonoBehaviour
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        playerInput.x = Input.GetAxisRaw("Horizontal");
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        if (playerInput.x != 0)
+        {
+            velocity.x += playerInput.x * acceleration * Time.deltaTime;
+            velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
+        }
+        else if (Mathf.Abs(velocity.x) > 0)
+        {
+            velocity.x += -Mathf.Sign(velocity.x) * deceleration * Time.deltaTime;
+        }
+        else
+        {
+            velocity.x = 0;
+        }
 
+        body.linearVelocity = velocity;
+    }
+
+    private void PlayerMovement()
+    {
+        
     }
 
     public bool IsWalking()
